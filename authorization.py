93403d1a994e2db543c9b09f770483df2a850d5d
Azuirith <AzuirithDev@gmail.com>
2024-03-26 19:52:33 -0400

    Separated authorization code into own file + code refactoring

---
+++ authorization.py
@@ -0,0 +1,69 @@
+from random import randint
+from time import sleep
+from selenium import webdriver
+from requests import post
+
+def generate_authorization_state(length):
+    state = ""
+    for _ in range(length): state += str(randint(0, 9))
+    return state
+
+def prompt_authorization_window(url):
+    # TODO: Account for case when code is never in url
+    browser = webdriver.Chrome()
+    browser.get(url)
+
+    # Waits until authorization request goes through
+    while not "code=" in browser.current_url: sleep(0.1)
+    authorized_url = browser.current_url
+    browser.close()
+
+    return authorized_url
+
+def get_query_parameter(url, parameter):
+    if not parameter in url: return None
+
+    # + 1 makes it start at the equal sign that precedes the parameter value
+    start_index = url.index(parameter) + len(parameter) + 1
+    parsed_parameter = url[start_index:]
+
+    end_index = None
+    if "&" in parsed_parameter: end_index = parsed_parameter.index("&")
+    else: end_index = len(parsed_parameter)
+    parsed_parameter = parsed_parameter[:end_index]
+
+    return parsed_parameter
+
+# TODO: Break this up into multiple functions
+def request_token(client_id, client_secret, redirect_uri): 
+    # Done for security check later on
+    state = generate_authorization_state(length=16) 
+
+    url = "https://accounts.spotify.com/authorize?"
+    url += f"client_id={client_id}&"
+    url += "response_type=code&"
+    url += f"redirect_uri={redirect_uri}&"
+    url += f"state={state}&"
+    url += "scope=playlist-modify-public"
+    
+    authorized_url = prompt_authorization_window(url)
+    returned_state = get_query_parameter(authorized_url, "state")
+    if (returned_state != state):
+        raise Exception("Error: Client state and server state do not match.")
+
+    # TODO 
+    authorization_code = get_query_parameter(authorized_url, "code")
+    url = "https://accounts.spotify.com/api/token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    body = {"grant_type": "authorization_code",
+            "code": authorization_code,
+            "redirect_uri": redirect_uri,
+            "client_id": client_id,
+            "client_secret": client_secret}
+
+    response = post(url, data=body, headers=headers)
+    STATUS_CODE_OK = 200
+    if response.status_code != STATUS_CODE_OK:
+        raise Exception("Error: Failed to grab authorization token.")
+    
+    return response.json()["access_token"]
--- main.py
+++ main.py
@@ -1,78 +1,14 @@
 from dotenv import load_dotenv
 from os import environ
-from random import randint
-from selenium import webdriver
-from time import sleep
-from requests import get, post
+from requests import post
+
+import authorization
 
 load_dotenv()
 CLIENT_ID = environ["CLIENT_ID"]
 CLIENT_SECRET = environ["CLIENT_SECRET"]
 REDIRECT_URI = environ["REDIRECT_URI"]
-
-def generate_authorization_state(length):
-    state = ""
-    for _ in range(length): state += str(randint(0, 9))
-    return state
-
-def prompt_authorization_window(url):
-    browser = webdriver.Chrome()
-    browser.get(url)
-
-    while not "code=" in browser.current_url: sleep(1)
-    authorized_url = browser.current_url
-    browser.close()
-
-    return authorized_url
-
-def get_url_header(url, header):
-    if not header in url: return None
-
-    # + 1 accounts for the equals sign that follows the header
-    start_index = url.index(header) + len(header) + 1
-    parsed_header = url[start_index:]
-
-    end_index = None
-    if ("&" in parsed_header): end_index = parsed_header.index("&")
-    else: end_index = len(parsed_header)
-    parsed_header = parsed_header[:end_index]
-
-    return parsed_header
-
-def request_token(): 
-    # Done for security check
-    state = generate_authorization_state(length=16) 
-
-    # URL headers must be added manually because URL is opened directly rather than using
-    # request library
-    url = "https://accounts.spotify.com/authorize?"
-    url += "client_id=" + CLIENT_ID + "&"
-    url += "response_type=code&"
-    url += "redirect_uri=" + REDIRECT_URI + "&"
-    url += "state=" + state + "&"
-    url += "scope=playlist-modify-public"
-    
-    authorized_url = prompt_authorization_window(url)
-    returned_state = get_url_header(authorized_url, "state")
-    if (returned_state != state):
-        raise Exception("Error: Client state and server state do not match.")
-
-    authorization_code = get_url_header(authorized_url, "code")
-
-    # TODO: Research http request bodies vs headers
-    url = "https://accounts.spotify.com/api/token"
-    body = {"grant_type": "authorization_code",
-            "code": authorization_code,
-            "redirect_uri": REDIRECT_URI,
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET}
-
-    response = post(url, data=body)
-    STATUS_CODE_OK = 200
-    if (not response.status_code == STATUS_CODE_OK):
-        raise Exception("Error: Failed to grab authorization token.")
-    
-    return response.json()["access_token"]
+PLAYLIST_ID = environ["PLAYLIST_ID"]
 
 def prompt_action_select():
     print("Select action:")
@@ -98,7 +34,15 @@ def remove_album(token, title):
     pass
 
 def add_song(token, title):
-    pass
+    song_uri = "spotify:track:4LB264fAdV4BPK99w4UMxl"
+
+    url = f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}/tracks?"
+    url += f"uris={song_uri}"
+    headers = {"Authorization": "Bearer " + token,
+               "Content-Type": "application/x-www-form-urlencoded"}
+    
+    response = post(url, headers=headers)
+    print(response.status_code)
 
 def remove_song(token, title):
     pass
@@ -106,7 +50,7 @@ def remove_song(token, title):
 print("### Music List Manager ###")
 
 print("Authorizing user...")
-token = request_token()
+token = authorization.request_token(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI)
 print("\nUser authorized.") # New line to space message from window library output
 
 program_running = True
