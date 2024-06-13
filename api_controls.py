1eef7382e2f01f646f87fa6d78db0aad6eae5f3a
Azuirith <AzuirithDev@gmail.com>
2024-06-12 21:07:38 -0400

    Implemented a rudimentary version of the remove_artist method among other small changes

--- api_controls.py
+++ api_controls.py
@@ -9,8 +9,16 @@ STATUS_CODE_OK = 200
 load_dotenv()
 PLAYLIST_ID = environ["PLAYLIST_ID"]
 
-request_headers = {} # This technically is sorta a constant 
-                     # but it does get changed once at the start of the program
+# NOTE: The add and remove methods could hypothetically access each other 
+# (Ex: add_artist calling add_album 3 times) but I felt that the program would be faster if it only
+# sent one request at a time, which such an implementation would require several calls for one artist
+# and potentially slow down the program greatly
+
+request_headers = {} # This technically is a constant but it does get changed once 
+                     # at the start of the program so it is not marked as one
+
+# TODO: Refactor this file so that names are consistent and also condense the sorting of the artists' top
+# albums into one function so that the code isn't repeating itself
 
 def create_request_headers(token):
     global request_headers
@@ -64,12 +72,51 @@ def add_artist(name):
 
     response = add_songs_from_uris(track_uris)
     if response.status_code != STATUS_CODE_OK:
-        print("Error: Failed to add requested artist to playlist\n")
+        print("Error: Failed to add requested artist to playlist.\n")
     else:
-        print("Artist added successfully\n")    
+        print("Artist added successfully.\n")    
 
-def remove_artist(token, name):
-    pass
+def remove_artist(name):
+    artist_item = search_for_item(name=name, item_type="artist")
+    artist_id = artist_item["id"]
+    
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/albums?"
+    url += "include_groups=album"
+
+    response = get(url, headers=request_headers)
+    returned_albums = response.json()["items"] 
+
+    album_ids = []
+    for album in returned_albums: album_ids.append(album["id"])
+
+    albums = []
+    for album_id in album_ids:
+        url = f"https://api.spotify.com/v1/albums/{album_id}"
+
+        response = get(url, headers=request_headers)
+        album_item = response.json()
+
+        album_name = album_item["name"]
+        album_popularity = album_item["popularity"]
+        albums.append((album_name, album_id, album_popularity))
+
+    albums = sorted(albums, key=lambda album: album[2], reverse=True) # TODO: write own sorting algorithm for funsies
+    top_albums = albums[:3]
+
+    track_uris = []
+    for album in top_albums:
+        url = f"https://api.spotify.com/v1/albums/{album[1]}/tracks"
+
+        response = get(url, headers=request_headers)
+        returned_tracks = response.json()["items"]
+
+        for track in returned_tracks: track_uris.append(track["uri"])
+
+    response = remove_songs_from_uris(track_uris)
+    if response.status_code != STATUS_CODE_OK:
+        print("Error: Failed to remove artist.\n")
+    else:
+        print("Artist removed successfully.\n")
 
 def add_album(name):
     album_item = search_for_item(name=name, item_type="album")
